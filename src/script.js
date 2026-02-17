00a73447a42a713b81f8f61da05a97fc28ba423d
LenahMithane <lenahkagisomithane@gmail.com>
2026-02-17 20:32:56 +0200

    Updated the code and added an img

--- src/script.js
+++ src/script.js
@@ -1,30 +1,30 @@
-function displayPoem(response) {
-  console.log("poem generated");
-  let poemElement = document.querySelector("#poem");
-  poemElement.innerHTML = response.data.answer;
+function displaylocation(response) {
+  console.log("hospital generated");
+  let hospitalElement = document.querySelector("#hospital");
+  hospitalElement.innerHTML = response.data.answer;
 }
 
 let apiKey="6010bf233cce4a37o06e80a3fbt0fe22";
 let instructionInput = document.querySelector("#user-instructions");
 
-function generatePoem(event) {
+function generatehospital(event) {
   event.preventDefault();
   
   let userInstructions = instructionInput.value;
-  let prompt = `user instructions are: Generate a french poem about ${userInstructions}`;
-  let context = `You are a romantic love poem expert and love to write short poems, Your mission is to generate a four line poem, Remember to follow the user instructions, make it simple and include a <br> in between line and do not include the heading`;
+  let prompt = `user instructions are: Generate the closest hospital to go to in ${userInstructions}`;
+  let context = `You are a helpful assistant that is always helping people locate closest hospital in cases of emergency, Your mission is to generate with accuracy a list of all the closest hospital to go to, Remember to follow the user instructions, make it simple and include a <br> in between line and do not include the heading and list only the hospitals and not other places in the area, do not forget to include the telephone number and the location of the hospital with a <br> in between hospitals and list a maximun of 2 hospital with accuracy.`;
   
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(prompt)}&context=${encodeURIComponent(context)}&key=${apiKey}`;
   
-  let poemElement = document.querySelector("#poem");
-  poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${userInstructions}</div>`;
+  let hospitalElement = document.querySelector("#hospital");
+  hospitalElement.classList.remove("hidden");
+  hospitalElement.innerHTML = `<div class="generating">⏳ Generating the closest hospital you can go to ${userInstructions}</div>`;
 
 
 
   
-  axios.get(apiURL).then(displayPoem);
+  axios.get(apiURL).then(displaylocation);
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+let hospitalFormElement = document.querySelector("#hospital-generator-form");
+hospitalFormElement.addEventListener("submit", generatehospital);
